b03c5bbd18268ea45bddbc0c020ab4da2714c13c
Zaur <zaurmag411@gmail.com>
2018-06-23 13:21:03 +0300

    +

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'), // подключаем Gulp
+    plugins = require('gulp-load-plugins')({
+        overridePattern: true,
+        pattern: '*'
+    }),
+    reqDir = require('require-dir');
+    reqDir('./gulp', { recurse: true });
+
+gulp.task('build', [
+    'pug:build',
+    'style:build',
+    'styleMin:build',
+    'img:build',
+    'jsConct:build',
+    'spriteImg:build',
+    'spriteSvg:build',
+    'fonts:build',
+    // Копирование файлов в шаблон CMS. Путь до шаблона меняется в файле - gulp/path.js
+    // 'copyStyles:build',
+    // 'copyScripts:build',
+    // 'copyImages:build',
+    // 'fontsCopyCms:build'
+]);
+
+// Запуск команд по умолчанию
+gulp.task('default', ['build', 'browser-sync', 'watch']);
